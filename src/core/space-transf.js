2769340544351bae52bf315b448900da65a6e171
Sylvain Plessis <sylvain.plessis@gmail.com>
2017-06-20 19:15:51 +0200

    space-trans up

--- src/core/space-transf.js
+++ src/core/space-transf.js
@@ -19,14 +19,14 @@ let m={};
 m.toC = (ds, data) => utils.homothe(ds.d.min,ds.c.min,ds.d2c,data);
 
 m.toCwidth = function(ds, dist){
-	let d = dist === undefined ? 1 : utils.toValue(dist);
+	let d = utils.isNil(dist) ? 1 : utils.toValue(dist);
 	return Math.abs(ds.d2c * d);
 };
 
 m.toD = (ds, coord) => utils.homothe(ds.c.min,ds.d.min,ds.c2d,coord);
 
 m.toDwidth = function(ds, dist){
-	let d = dist === undefined ? 1 : utils.toValue(dist);
+	let d = utils.isNil(dist) ? 1 : utils.toValue(dist);
 	return Math.abs(ds.c2d * d);
 };
 
