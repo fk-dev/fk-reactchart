bf37e7dcd7069ad7079138a6a55c46cbb6cc1d66
Sylvain Plessis <sylvain.plessis@gmail.com>
2019-01-16 17:33:18 +0100

    icons can use gradients

--- src/icons/iconer.jsx
+++ src/icons/iconer.jsx
@@ -2,55 +2,56 @@ import React from 'react';
 
 let _icon = {};
 
-_icon.square = _icon.Square = function(data,open){
-	let l = Math.min(data.width, data.height) * 3/5;
-	let x = data.hMargin + (data.width - l)/2 ;
-	let y = data.vMargin + (data.height - l);
-	let f = open ? 'none' : data.color;
+_icon.square = _icon.Square = function(data,style,open){
+	const l = Math.min(data.width, data.height) * 3/5;
+	const x = data.hMargin + (data.width - l)/2 ;
+	const y = data.vMargin + (data.height - l);
+	const f = open ? 'none' : style ? `url(#${style}` : data.color;
 	return <rect x={x} y={y} width={l} height={l} fill={f} stroke={data.color} />;
 };
 
-_icon.opensquare = _icon.OpenSquare = (data) => _icon.square(data,true);
+_icon.opensquare = _icon.OpenSquare = (data, style) => _icon.square(data,style, true);
 
-_icon.dot = _icon.Dot = function(data,open){
-	let x = (data.width + 2 * data.hMargin)/2;
-	let r = Math.min(data.height, data.width) * 3 / 10; // 3 / 5 de remplissage
-	let y = data.height + data.vMargin - r;
-	let f = open ? 'none' : data.color;
+_icon.dot = _icon.Dot = function(data,style, open){
+	const x = (data.width + 2 * data.hMargin)/2;
+	const r = Math.min(data.height, data.width) * 3 / 10; // 3 / 5 de remplissage
+	const y = data.height + data.vMargin - r;
+	const f = open ? 'none' : style ? `url(#${style})` : data.color;
 	return <circle cx={x} cy={y} r={r} fill={f} stroke={data.color}/>;
 };
 
-_icon.opendot = _icon.OpenDot = (data) => _icon.dot(data,true);
+_icon.opendot = _icon.OpenDot = (data,style) => _icon.dot(data,style,true);
 
 _icon.bar = _icon.Bar = _icon.square;
 
-_icon.pie = _icon.Pie = function(data){
-	let x = data.hMargin + data.width/2;
-	let y = 2 * data.vMargin + data.height;
-	let r = data.height;
-	let x1 = x + r * Math.cos(3/8 * Math.PI);
-	let y1 = y - r * Math.sin(3/8 * Math.PI);
-	let x2 = x + r * Math.cos(5/8 * Math.PI);
-	let y2 = y - r * Math.sin(5/8 * Math.PI);
+_icon.pie = _icon.Pie = function(data,style){
+	const x = data.hMargin + data.width/2;
+	const y = 2 * data.vMargin + data.height;
+	const r = data.height;
+	const x1 = x + r * Math.cos(3/8 * Math.PI);
+	const y1 = y - r * Math.sin(3/8 * Math.PI);
+	const x2 = x + r * Math.cos(5/8 * Math.PI);
+	const y2 = y - r * Math.sin(5/8 * Math.PI);
+	const f = style ? `url(#${style})` : data.color;
 	
-	let path = 'M' + x + ',' + y + ' L' + x1 + ',' + y1 + ' A' + r + ',' + r + ' 0 0,0 ' + x2 + ',' + y2 + ' z';
-	return <path fill={data.color} d={path}/>;
+	const path = 'M' + x + ',' + y + ' L' + x1 + ',' + y1 + ' A' + r + ',' + r + ' 0 0,0 ' + x2 + ',' + y2 + ' z';
+	return <path fill={f} d={path}/>;
 };
 
 _icon.line = function(data){
 
-	let l = Math.min(data.width, data.height);
-	let x1 = data.hMargin + (data.width - l)/2 ;
-	let x2 = x1 + l;
-	let y = data.vMargin + (data.height - 6); // fraction of height of letters...
+	const l = Math.min(data.width, data.height);
+	const x1 = data.hMargin + (data.width - l)/2 ;
+	const x2 = x1 + l;
+	const y = data.vMargin + (data.height - 6); // fraction of height of letters...
 	return <line x1={x1} y1={y} x2={x2} y2={y} stroke={data.color} strokeWidth={data.strokeWidth}/>;
 };
 
 
-export function iconer(data, key){
+export function iconer(data, key, style){
 	if(!_icon[key]){
 		throw new Error('unrecognized mark type: "' + key + '"');
 	}
 
-	return _icon[key](data);
+	return _icon[key](data,style);
 }
