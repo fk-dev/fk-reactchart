9ed287aeda6492fe15259c9cc946ebf14dda0a83
Sylvain Plessis <sylvain.plessis@gmail.com>
2020-06-09 14:26:45 +0200

    test env updated

--- gulpFile.js
+++ gulpFile.js
@@ -1,31 +1,32 @@
-var gulp = require('gulp');
+const { series, parallel, src, dest } = require('gulp');
 // js
-var browserify = require('browserify');
-//var uglify     = require('gulp-uglify');
-var terser     = require('gulp-terser');
-var header     = require('gulp-header');
-var replace    = require('gulp-replace');
+const browserify = require('browserify');
+//const uglify     = require('gulp-uglify');
+const terser     = require('gulp-terser');
+const header     = require('gulp-header');
+const replace    = require('gulp-replace');
 
-var collapse   = require('bundle-collapser/plugin');
-var source     = require('vinyl-source-stream');
-var buffer     = require('vinyl-buffer');
+const collapse   = require('bundle-collapser/plugin');
+const source     = require('vinyl-source-stream');
+const buffer     = require('vinyl-buffer');
+const mocha      = require('gulp-mocha');
 
 /////////
 /// pages
 /////
 
-var rootdirname = __dirname;
+const rootdirname = __dirname;
 
-gulp.task('page-js', function(){
-  var headerTxt = '/* ' + new Date().getUTCFullYear() + '- generated at ' + new Date() + '\n*/';
-  var browserifyOption = {
+function pageJs(){
+  const headerTxt = '/* ' + new Date().getUTCFullYear() + '- generated at ' + new Date() + '\n*/';
+  const browserifyOption = {
       entries: './pages/pages.js',
       extensions: ['.js'],
       //debug: true,
     };
   
-    var b = browserify(browserifyOption);
-    var bundler = b
+    const b = browserify(browserifyOption);
+    let bundler = b
       .transform('partialify')
       .transform('babelify', {
         compact: "auto",
@@ -33,41 +34,41 @@ gulp.task('page-js', function(){
           presets: ['es2015', 'react']
         });
     
-    var rebundle = function() {
-      var rc = bundler.plugin(collapse)
+    const rebundle = function() {
+      const rc = bundler.plugin(collapse)
         .bundle()
         .pipe(source('page.js'))
         .pipe(buffer())
     //    .pipe(uglify())
         .pipe(header(headerTxt))
-        .pipe(gulp.dest(rootdirname));
+        .pipe(dest(rootdirname));
   
         return rc;
       };
     bundler = bundler.on('update', rebundle);
     return rebundle();
-});
+}
   
-gulp.task('page-css', function(){
-  return 0;/*gulp.src("./pages/pages.less")
+function pageCSS(){
+  return 0;/*src("./pages/pages.less")
     .pipe(less())
     .pipe(rename('page.css'))
-    .pipe(gulp.dest(rootdirname));*/
-});
+    .pipe(dest(rootdirname));*/
+}
 
 /////////
 // dist
 /////////
-gulp.task('full', function(){
+function full(){
 
-  var headerTxt = '/* ' + new Date().getUTCFullYear() + '- generated at ' + new Date() + '\n*/';
-  var browserifyOption = {
+  const headerTxt = '/* ' + new Date().getUTCFullYear() + '- generated at ' + new Date() + '\n*/';
+  const browserifyOption = {
     entries: './src/Graph.jsx',
     extensions: ['.js','.jsx']
   };
   
-  var b = browserify(browserifyOption);
-  var bundler = b
+  const b = browserify(browserifyOption);
+  let bundler = b
     .transform('partialify')
     .transform('babelify', {
       compact: "auto",
@@ -75,31 +76,31 @@ gulp.task('full', function(){
       presets: ['es2015', 'react']
   }).external('react');
   
-  var rebundle = function() {
-    var rc = bundler.plugin(collapse)
+  const rebundle = function() {
+    const rc = bundler.plugin(collapse)
       .bundle()
       .pipe(source('reactchart.js'))
       .pipe(buffer())
       .pipe(header(headerTxt))
-      .pipe(gulp.dest('./dist'));
+      .pipe(dest('./dist'));
   
       return rc;
     };
   bundler = bundler.on('update', rebundle);
   return rebundle();
-});
+}
 
-gulp.task('min', function(){
+function min(){
 
-  var headerTxt = '/* ' + new Date().getUTCFullYear() + '- generated at ' + new Date() + '\n*/';
-  var browserifyOption = {
+  const headerTxt = '/* ' + new Date().getUTCFullYear() + '- generated at ' + new Date() + '\n*/';
+  const browserifyOption = {
     entries: './src/Graph.jsx',
     extensions: ['.js','.jsx']
     //debug: true,
   };
   
-  var b = browserify(browserifyOption);
-  var bundler = b
+  const b = browserify(browserifyOption);
+  let bundler = b
     .transform('partialify')
     .transform('babelify', {
       compact: "auto",
@@ -107,8 +108,8 @@ gulp.task('min', function(){
       presets: ['es2015', 'react']
   }).external('react');
   
-  var rebundle = function() {
-    var rc = bundler.plugin(collapse)
+  const rebundle = function() {
+    const rc = bundler.plugin(collapse)
       .bundle()
       .pipe(source('reactchart.min.js'))
       .pipe(buffer())
@@ -118,56 +119,76 @@ gulp.task('min', function(){
         }
       }))
       .pipe(header(headerTxt))
-      .pipe(gulp.dest('./dist'));
+      .pipe(dest('./dist'));
   
       return rc;
     };
   bundler = bundler.on('update', rebundle);
   return rebundle();
-});
+}
 
 ////////////
 // jsx -> js in require
 ///////////
 
-gulp.task('jsx2js', () => {
-  return gulp.src(['.lib/*.js',"./lib/**/*.js"])
+function jsx2js(){
+  return src(['.lib/*.js',"./lib/**/*.js"])
     .pipe(replace(/\.jsx/g,'.js'))
-    .pipe(gulp.dest('./lib'));
-});
+    .pipe(dest('./lib'));
+}
 
 ////////////
 // src -> lib in index
 ///////////
 
-gulp.task('src2libIdx', () => {
-  return gulp.src(["./index.js"])
+function src2libIdx(){
+  return src(["./index.js"])
     .pipe(replace(/src/g,'lib'))
-    .pipe(gulp.dest('.'));
-});
+    .pipe(dest('.'));
+}
 
-gulp.task('src2libHlp', () => {
-  return gulp.src(["./helpers/index.js"])
+function src2libHlp() {
+  return src(["./helpers/index.js"])
     .pipe(replace(/src/g,'lib'))
-    .pipe(gulp.dest('./helpers'));
-});
-
-gulp.task('src2lib',["src2libIdx","src2libHlp"],() => 0);
+    .pipe(dest('./helpers'));
+}
 
 //////////////
 // .npmignore ignores src/
 /////////////
 
-gulp.task('noSrc',() => {
-  return gulp.src(['./.npmignore'])
+function noSrc(){
+  return src(['./.npmignore'])
     .pipe(header('src\n'))
-    .pipe(gulp.dest('.'));
-});
+    .pipe(dest('.'));
+}
 
-gulp.task('buildNpm',['jsx2js','src2lib','noSrc'],() => 0);
 
-gulp.task('dist',['full','min'],() => 0);
+////////////////
+/// tests
+//////////////
 
-gulp.task('doc',['page-js','page-css'],() => 0);
+const mochaOptions = {
+  globals: [
+    'expect', 'sinon', 'v2', 'v3'
+  ],
+  ignoreLeaks: false
+};
 
-gulp.task('default',['dist','doc'],() => 0);
+const doTest = () => src(['./test/*.test.js'], { allowEmpty: true })
+	.pipe(mocha(mochaOptions));
+
+/////
+
+exports.test = doTest;
+
+exports.buildNpm = series(jsx2js,parallel(src2libIdx,src2libHlp),noSrc);
+
+exports.dist = series(full,min);
+
+exports.doc = parallel(pageJs,pageCSS);
+
+exports.default = parallel(series(full,min),parallel(pageJs,pageCSS));
+
+	
+
